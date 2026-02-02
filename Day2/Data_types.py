5250cad627123e85006c29c27f2e488b6215399c
Fatma Tugce BULUT <bulutfatmatugce@yahoo.com>
2026-02-02 21:40:50 +0300

    day2 added

---
+++ Day2/Data_types.py
@@ -0,0 +1,25 @@
+#Stitrings
+print("Strings")
+
+#Integers
+print(1234)
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+#Floats
+print(123.2131)
+
+#Booleans
+print(True)
+print(False)
+
+#Modulla
+print(7/3)
+print(7%3)
---
+++ Day2/Type_eror_checking_and_coversion.py
@@ -0,0 +1,15 @@
+# len(12345)
+# len("fatma")
+
+#Type Checking
+# print(type("fatma"))
+# print(type(1234))
+# print(type(123.456))
+# print(type(False))
+
+# print("Number of letters in your name: " + len(input("Enter your name")))
+name_of_user = input("Enter your name: ")
+length_of_name = len(name_of_user)
+# print(type("Enter your name: ")) #str
+# print(type(length_of_name)) #int
+print("Number ıf letters in your name: "+str(length_of_name))
